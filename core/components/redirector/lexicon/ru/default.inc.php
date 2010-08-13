635844e7513ddcbc0ecfdba4b3469932b46e9322
Shaun McCormick <shaun@modxcms.com>
2010-08-12 23:10:28 -0500

    Added Russian translation

---
+++ core/components/redirector/lexicon/ru/default.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Default Russian Lexicon Entries for redirector
+ *
+ * @package redirector
+ * @subpackage lexicon
+ */
+$_lang['redirector'] = 'Redirector';
+$_lang['redirector.active'] = 'Включён';
+$_lang['redirector.desc'] = 'Управление редиректами на вашем сайте.';
+$_lang['redirector.description'] = 'Описание';
+$_lang['redirector.redirect_err_ae'] = 'Редирект с таким именем уже есть.';
+$_lang['redirector.redirect_err_nf'] = 'Редирект не найден.';
+$_lang['redirector.redirect_err_ns'] = 'Редирект не указан.';
+$_lang['redirector.redirect_err_ns_name'] = 'Укажите имя редиректа.';
+$_lang['redirector.redirect_err_remove'] = 'Произошла ошибка при попытке удалить редирект.';
+$_lang['redirector.redirect_err_save'] = 'Произошла ошибка при попытке сохранить редирект.';
+$_lang['redirector.redirect_create'] = 'Создать новый редирект';
+$_lang['redirector.redirect_remove'] = 'Удалить редирект';
+$_lang['redirector.redirect_remove_confirm'] = 'Вы уверены, что хотите удалить этот редирект?';
+$_lang['redirector.redirect_update'] = 'Обновить редирект';
+$_lang['redirector.redirects'] = 'Редиректы';
+$_lang['redirector.management'] = 'Управление редиректами';
+$_lang['redirector.menu_desc'] = 'Управление редиректами.';
+$_lang['redirector.pattern'] = 'Шаблон';
+$_lang['redirector.search...'] = 'Поиск...';
+$_lang['redirector.target'] = 'Цель';
+
