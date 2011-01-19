e085767ef8d3720944be800833509f6ecf56a0a6
smooth-graphics <info@novolo.de>
2011-01-19 10:22:25 +0100

    added german translation
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/redirector/lexicon/de/default.inc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Default English Lexicon Entries for redirector
+ *
+ * @package redirector
+ * @subpackage lexicon
+ * @author smooth-graphics (Anselm Hannemann)
+ */
+$_lang['redirector'] = 'Redirector';
+$_lang['redirector.active'] = 'Aktiv';
+$_lang['redirector.desc'] = 'Verwalten Sie Umleitungen für Ihre Webseite.';
+$_lang['redirector.description'] = 'Beschreibung';
+$_lang['redirector.redirect_err_ae'] = 'Es existiert bereits eine Umleitung mit diesem Namen.';
+$_lang['redirector.redirect_err_nf'] = 'Umleitung nicht gefunden.';
+$_lang['redirector.redirect_err_ns'] = 'Keine Umleitung erstellt.';
+$_lang['redirector.redirect_err_ns_name'] = 'Bitte geben Sie einen Namen für die Umleitung an.';
+$_lang['redirector.redirect_err_remove'] = 'Es ist ein Fehler beim Entfernen der Umleitung aufgetreten.';
+$_lang['redirector.redirect_err_save'] = 'Es ist ein Fehler beim Speichern der Umleitung aufgetreten.';
+$_lang['redirector.redirect_create'] = 'neue Umleitung erstellen';
+$_lang['redirector.redirect_remove'] = 'Umleitung entfernen';
+$_lang['redirector.redirect_remove_confirm'] = 'Sind Sie sicher, dass Sie die Umleitung entfernen möchten?';
+$_lang['redirector.redirect_update'] = 'Umleitung bearbeiten';
+$_lang['redirector.redirects'] = 'Umleitungen';
+$_lang['redirector.management'] = 'Umleitungsverwaltung';
+$_lang['redirector.menu_desc'] = 'Verwalten Sie Umleitungen für Ihre Webseite.';
+$_lang['redirector.pattern'] = 'Muster';
+$_lang['redirector.search...'] = 'Suche...';
+$_lang['redirector.target'] = 'Ziel';
+
