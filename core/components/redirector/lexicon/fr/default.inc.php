75ae8e651b48eeb58c1f9b4d25fe884cd2b8ff88
Romain Tripault <romain@meltingmedia.net>
2011-05-17 18:52:06 +0200

    French translation

---
+++ core/components/redirector/lexicon/fr/default.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Default French Lexicon Entries for redirector
+ *
+ * @package redirector
+ * @subpackage lexicon
+ */
+$_lang['redirector'] = 'Redirector';
+$_lang['redirector.active'] = 'Actif';
+$_lang['redirector.desc'] = 'Gérez les redirections de votre site internet.';
+$_lang['redirector.description'] = 'Description';
+$_lang['redirector.redirect_err_ae'] = 'Une redirection portant ce nom existe déjà.';
+$_lang['redirector.redirect_err_nf'] = 'Redirection non trouvée.';
+$_lang['redirector.redirect_err_ns'] = 'Redirection non spécifiée.';
+$_lang['redirector.redirect_err_ns_name'] = 'Veuillez indiquer un nom pour la redirection.';
+$_lang['redirector.redirect_err_remove'] = 'Une erreur est survenue lors de la suppression de la redirection.';
+$_lang['redirector.redirect_err_save'] = 'Une erreur est survenue lors de la sauvegarde de la redirection.';
+$_lang['redirector.redirect_create'] = 'Créer une nouvelle redirection';
+$_lang['redirector.redirect_remove'] = 'Supprimer la redirection';
+$_lang['redirector.redirect_remove_confirm'] = 'Êtes-vous sûr de vouloir supprimer cette redirection ?';
+$_lang['redirector.redirect_update'] = 'Mettre à jour la redirection';
+$_lang['redirector.redirects'] = 'Redirections';
+$_lang['redirector.management'] = 'Gestion des redirections';
+$_lang['redirector.menu_desc'] = 'Gérez vos redirections.';
+$_lang['redirector.pattern'] = 'Modèle';
+$_lang['redirector.search...'] = 'Chercher…';
+$_lang['redirector.target'] = 'Cible';
