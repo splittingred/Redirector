f857e2be0c4f6aed4add69bc6fc5a19ae614eda5
Romain <romain@melting-media.com>
2012-07-25 18:12:24 +0200

    Fix issue where hitting enter key on the search textfield resulted in a getValue error

--- assets/components/redirector/js/mgr/widgets/redirects.grid.js
+++ assets/components/redirector/js/mgr/widgets/redirects.grid.js
@@ -42,7 +42,7 @@ Redi.grid.Redirects = function(config) {
                     new Ext.KeyMap(cmp.getEl(), {
                         key: Ext.EventObject.ENTER
                         ,fn: function() {
-                            this.fireEvent('change',this.getValue());
+                            this.fireEvent('change');
                             this.blur();
                             return true; }
                         ,scope: cmp
@@ -59,7 +59,7 @@ Redi.grid.Redirects = function(config) {
 Ext.extend(Redi.grid.Redirects,MODx.grid.Grid,{
     search: function(tf,nv,ov) {
         var s = this.getStore();
-        s.baseParams.query = tf.getValue();
+        s.baseParams.query = Ext.getCmp('redirector-search-filter').getValue();
         this.getBottomToolbar().changePage(1);
         this.refresh();
     }
